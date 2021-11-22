17f5025c8164c908bbc89c55150e0fab3b8192e5
Vladislav <ivanovlad2010@gmail.com>
2021-11-22 09:38:40 +0300

    fundamentals: loop pattern added

--- 01_fundamentals/script.js
+++ 01_fundamentals/script.js
@@ -1,4 +1,4 @@
-const canvas = document.querySelector(".canvas");
+const canvas = document.querySelector(".initial-canvas");
 const context = canvas.getContext("2d");
 
 console.log(context);
@@ -15,3 +15,37 @@ context.beginPath();
 context.arc(300, 300, 100, 0, Math.PI * 2);
 context.stroke();
 context.closePath();
+
+const loopCanvas = document.querySelector(".loop-canvas");
+const loopContext = loopCanvas.getContext("2d");
+
+loopContext.lineWidth = 4;
+
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        const width = 80;
+        const height = width;
+        const gap = 12;
+
+        loopContext.beginPath();
+        loopContext.rect(100 + i * (width + gap), 100 + j * (height + gap), width, height);
+        loopContext.stroke();
+        loopContext.closePath();
+
+        if (Math.random() > 0.5) {
+            continue;
+        }
+
+        const innerRectOffset = 8;
+
+        loopContext.beginPath();
+        loopContext.rect(
+            100 + i * (width + gap) + innerRectOffset,
+            100 + j * (height + gap) + innerRectOffset,
+            width - innerRectOffset * 2,
+            height - innerRectOffset * 2
+        );
+        loopContext.stroke();
+        loopContext.closePath();
+    }
+}
